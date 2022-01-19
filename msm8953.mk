d7956c07ad3dccd4d538987157699c17e4f4a8c9
Undying-yueyue <orgtech007@gmail.com>
2022-01-19 12:07:03 +0100

    msm8953-common: Set Maintainer informations for Miku UI

--- msm8953.mk
+++ msm8953.mk
@@ -10,9 +10,16 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Signed
 -include vendor/extra/product.mk
 
+# Maintaier
+MIKU_MASTER := RenzAlt
+
+# Miku UI OFFICIAL
+TARGET_MIKU_BUILD_VARIANT := UNOFFICIAL
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-miku
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
