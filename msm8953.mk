a43f6a2c0cbe5e87828515a5499d46b949a8356e
me-cafebabe <me.cafebabe@gmail.com>
2023-02-21 21:25:09 +0800

    msm8953-common: Add libgui shim for lib-imscamera.so
    * R lib-imscamera.so have missing symbol on S, shim it

--- msm8953.mk
+++ msm8953.mk
@@ -221,6 +221,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0
 
+PRODUCT_PACKAGES += \
+    libgui_shim
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
