2472c97971a283a129a8871baae2265107aa6421
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-02-11 20:09:31 +0000

    msm8953-common: migrate to FCM 5
    We have the 4.19 kernel, which meets the target FCM 5 requirements.
    
    on-behalf-of: @Alts-Project <Alts-Project@organization.com>

--- msm8953.mk
+++ msm8953.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+# disable_configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
