65025bada3255b993fe9724ce941d8abd963af97
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-10 06:29:57 +0000

    msm8953-common: Remove Lineage HALs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,9 +102,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_compatibility_matrix.xml 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
