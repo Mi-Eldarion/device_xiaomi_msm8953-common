9e2c306ba0e0f83632c4a3cb11d7b412ed1a8c1a
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-10 14:55:41 +0000

    msm8953-common: Inherit MiTitanium QCOM HALs
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,7 +132,11 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/xiaomi_msm8953_touchscreen/enable_dt2w
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msm8953
-TARGET_ENFORCES_QSSI := true
+ifeq ($(USE_MITITANIUM_HALS),true)
+QCOM_SOONG_NAMESPACE := hardware/mititanium-$(TARGET_KERNEL_VERSION)
+else
+TARGET_ENFORCE_QSSI := true
+endif
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
