7218e637c4516e86af50df001ba42f817f99d8a9
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-10 06:46:11 +0000

    msm8953-common: drop lineage Trust HAL

--- msm8953.mk
+++ msm8953.mk
@@ -384,10 +384,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_mi8953
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
